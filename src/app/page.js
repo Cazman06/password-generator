dc49e9b4d4329ebd4e53aa15080c3212db4fd222
Cazman06 <61445651+Cazman06@users.noreply.github.com>
2025-01-29 21:47:31 -0600

    Allows characters and numbers to be toggled during password generation (#3)

--- src/app/page.js
+++ src/app/page.js
@@ -9,6 +9,7 @@ import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid2';
 import { styled } from '@mui/material';
+import Switch from '@mui/material/Switch';
 
 const StyledContainer = styled(Box)`
   display: flex;
@@ -95,21 +96,84 @@ const StyledButton = styled(Button)`
   }
 `;
 
-const generatePassword = (length) => {
-  const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+[]{}|;:,.<>?";
+const StyledSwitch = styled(Switch)`
+  && {
+    .MuiSwitch-switchBase {
+      color: #FF677D;
+      &:hover {
+        background-color: rgba(255, 103, 125, 0.08);
+      }
+    }
+
+    .MuiSwitch-track {
+      background-color: #ddd;
+    }
+
+    .MuiSwitch-thumb {
+      background-color: #FF677D;
+    }
+
+    .Mui-checked {
+      color: #3f51b5;
+    }
+
+    .Mui-checked + .MuiSwitch-track {
+      background-color: #3f51b5;
+    }
+  }
+`;
+
+const StyledLabel = styled(Typography)`
+  font-size: 16px;
+  color: #333;
+  margin-bottom: 10px;
+  margin-top: 5px;
+`;
+
+const StyledSwitchContainer = styled(Box)`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  margin-bottom: 20px;
+`;
+
+const LETTERS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const CHARACTERS = '!@#$%^&*()_+[]{}|;:,.<>?'
+const NUMBERS = '0123456789'
+
+const generatePassword = (length, includeCharacters, includeNumbers) => {
   const array = new Uint32Array(length);
-  window.crypto.getRandomValues(array);
-  return array.map(val => charset[val % charset.length]).join('');
+  const randomValues = window.crypto.getRandomValues(array);
+
+  const charSet = [
+    LETTERS,
+    includeCharacters ? CHARACTERS : '',
+    includeNumbers ? NUMBERS : ''
+  ].join('')
+
+  return Array.from(randomValues).map(val => charSet.charAt(val % charSet.length)).join('')
 };
 
 const PasswordGenerator = () => {
   const [password, setPassword] = useState('');
   const [length, setLength] = useState(20);
+  const [includeCharacters, setIncludeCharacters] = useState(false);
+  const [includeNumbers, setIncludeNumbers] = useState(false);
+
 
   const handleGenerateClick = () => {
-    const newPassword = generatePassword(length);
+    const newPassword = generatePassword(length, includeCharacters, includeNumbers);
     setPassword(newPassword);
   };
+
+  const handleCharacterChange = (event) => {
+    setIncludeCharacters(event.target.checked)
+  }
+
+  const handleNumberChange = (event) => {
+    setIncludeNumbers(event.target.checked)
+  }
   
   const handleCopyToClipboardClick = async (text) => {
     await navigator.clipboard.writeText(text);
@@ -130,6 +194,26 @@ const PasswordGenerator = () => {
           onChange={handleSliderChange}
           valueLabelDisplay="auto"
         />
+
+        <StyledSwitchContainer>
+
+        <StyledLabel> Characters </StyledLabel>
+
+        <StyledSwitch
+          checked={includeCharacters}
+          onChange={(e) => handleCharacterChange(e)}
+          inputProps={{ 'aria-label': 'controlled' }}
+        />
+
+        <StyledLabel> Numbers </StyledLabel>
+
+        <StyledSwitch
+          checked={includeNumbers}
+          onChange={(e) => handleNumberChange(e)}
+          inputProps={{ 'aria-label': 'controlled' }}
+        />
+
+        </StyledSwitchContainer>
 
         <StyledTextField
           label="Generated Password"
